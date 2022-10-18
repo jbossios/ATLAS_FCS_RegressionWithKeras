2966d134279e81aacc2bd634cc839d4c4eee21ec
jbossios <bossjonad@gmail.com>
2022-10-18 10:52:47 +0200

    Add R22 inputs

--- InputFiles.py
+++ InputFiles.py
@@ -8,6 +8,20 @@ PATH2InputFiles = { # collect the paths where all files are available for each t
 # First (incomplete) versions for full detector
 #  'photons'             : '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/MicheleInputsCSV/photons/v8/',
 # Complete versions for full detector
-  'photons' : '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/MicheleInputsCSV/photons/v9/',
-  'pions' : '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/MicheleInputsCSV/pions/full_detector/',
+#  'photons': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/MicheleInputsCSV/photons/v10/',
+#  'electrons': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/MicheleInputsCSV/electrons/full_detector/v3/',
+#  'electronsANDphotons': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/MicheleInputsCSV/electrons_and_photons/full_detector/v2/',
+#  'pions': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/MicheleInputsCSV/pions/full_detector_v2/',
+# Testing (new inputs but using old inputs from pions)
+#  'pions' : '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/MicheleInputsCSV_test/pions/',
+# R22 inputs (v0: upto |eta|<1.0)
+  #'photons': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/R22/MicheleInputsCSV/photons/v0/',
+  #'electrons': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/R22/MicheleInputsCSV/electrons/v0/',
+  #'electronsANDphotons': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/R22/MicheleInputsCSV/electrons_and_photons/v0/',
+  #'pions': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/R22/MicheleInputsCSV/pions/v0/',
+# R22 inputs (v1: upto |eta|<1.0)
+  'photons': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/R22/MicheleInputsCSV/photons/v1/',
+  'electrons': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/R22/MicheleInputsCSV/electrons/v1/',
+  'electronsANDphotons': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/R22/MicheleInputsCSV/electrons_and_photons/v1/',
+  'pions': '/eos/atlas/atlascerngroupdisk/proj-simul/AF3_Run3/Jona/R22/MicheleInputsCSV/pions/v1/',
 }
